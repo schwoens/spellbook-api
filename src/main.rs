991cfb3fea5b2228fa9a1f1c9687620cf176e6d6
dtoews <d.toews@emondo.de>
2025-03-27 16:13:02 +0100

    add cors layer

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,13 @@ use spellbook_api::handlers::{
     },
     users::post_user,
 };
+use tower_http::cors::{CorsLayer, Any};
 
 #[tokio::main]
 async fn main() {
+
+    let cors_layer = CorsLayer::new().allow_origin(Any);
+
     let app = Router::new()
         .route("/spells", get(get_spells).post(post_spell))
         .route("/spells/query", post(query_spells))
@@ -25,7 +29,8 @@ async fn main() {
         .route("/public/spells/query", post(query_public_spells))
         .route("/public/spell/copy/:nanoid", patch(copy_public_spell))
         .layer(middleware::from_fn(spellbook_api::middleware::auth))
-        .route("/users", post(post_user));
+        .route("/users", post(post_user))
+        .layer(cors_layer);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("listening on port 3000...");
